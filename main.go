7b3a4248df2fca0e507e452ca005d17ab1f38eb8
lakoth <lornaakothpb@gmail.com>
2024-08-20 14:47:01 +0300

    Fix:handled suppose banners do not exist

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"ASCII-WEB/ascii-art/printingasciipackage"
 	"fmt"
 	"net/http"
 	"text/template"
+
+	"ASCII-WEB/ascii-art1/printingasciipackage"
 )
 
 func main() {
@@ -12,14 +13,11 @@ func main() {
 	http.HandleFunc("/ascii-art", GenerateArt)
 	fmt.Println("Starting server on port 8000")
 	err := http.ListenAndServe(":8000", nil)
-
 	if err != nil {
 		fmt.Println("Server failed to start")
 		return
 	}
-
 }
-
 
 type ArtDetails struct {
 	UserInput  string
@@ -34,22 +32,22 @@ func HomePageHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if r.Method != http.MethodGet{
-		http.ServeFile(w,r,"405.html")
+	if r.Method != http.MethodGet {
+		http.ServeFile(w, r, "405.html")
 		return
 	}
 
 	tmpl.Execute(w, nil)
 }
 
-func GenerateArt(w http.ResponseWriter, r *http.Request){
+func GenerateArt(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("index.html")
-	if err !=nil{
+	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	if r.Method !=http.MethodPost{
-		http.ServeFile(w,r,"405.html")
+	if r.Method != http.MethodPost {
+		http.ServeFile(w, r, "405.html")
 		return
 	}
 	if err := r.ParseForm(); err != nil {
@@ -58,12 +56,22 @@ func GenerateArt(w http.ResponseWriter, r *http.Request){
 	}
 	text := r.FormValue("userInput")
 	banner := r.FormValue("banners")
+	fmt.Print(text)
 
 	if text == "" {
-		http.ServeFile(w,r,"400.html")
+		http.ServeFile(w, r, "400.html")
 		return
 	}
-	result1 := printingasciipackage.PrintingAscii(text, banner)
+	if banner == "" {
+		http.ServeFile(w, r, "400.html")
+		return
+	}
+
+	result1, err := printingasciipackage.PrintingAscii(text, banner)
+	if err != nil {
+		http.ServeFile(w, r, "404.html")
+		return
+	}
 
 	neededData := ArtDetails{
 		UserInput:  text,
@@ -71,8 +79,8 @@ func GenerateArt(w http.ResponseWriter, r *http.Request){
 		Result:     result1,
 	}
 
-	err = tmpl.Execute(w, neededData) 
-	if err!=nil{
+	err = tmpl.Execute(w, neededData)
+	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
