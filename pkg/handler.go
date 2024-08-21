7ccbcdc07af1b029b7b872fbbaa642c937db3782
fnamayi <franklynenm@gmail.com>
2024-08-21 06:09:32 +0300

    feat:(handle bad method #line17 homehundler func)

--- pkg/handler.go
+++ pkg/handler.go
@@ -7,10 +7,22 @@ import (
 
 var banners = []string{"shadow.txt", "standard.txt", "thinkertoy.txt"}
 
+// handles http requests and responses
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" && r.URL.Path != "/ascii-art" {
+		http.ServeFile(w, r, "404.html")
+		return
+	}
+	//check the HTTP method
+	if r.Method != http.MethodGet && r.Method != http.MethodPost {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	tmpl, err := template.ParseFiles("templates/index.html")
 	if err != nil {
-		http.Error(w, "Not Found", http.StatusNotFound)
+		http.Error(w, "internal server Error", http.StatusNotFound)
 		return
 	}
 	tmpl.Execute(w, banners)
