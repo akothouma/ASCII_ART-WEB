37cc79ab64c6a806aca0081f75224b7447b87ef3
fnamayi <franklynenm@gmail.com>
2024-08-21 06:11:10 +0300

    fix:(removed unnecessary comments in main.go

--- main.go
+++ main.go
@@ -6,18 +6,13 @@ import (
 	"net/http"
 )
 
-// Home page handler
-
-// Main function
 func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/", pkg.HomeHandler)
 	http.HandleFunc("/ascii-art", pkg.AsciiArtHandler)
-
-	// fmt.Println("Server is running on :8080")
 	fmt.Println("Server is running on http://localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Println("Error starting server:", err)
+		fmt.Println("error: Starting server:", err)
 	}
 }
