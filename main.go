bca1eebe5fec4b3f950c1f93dfb15a6c211e59d5
lakoth <lornaakothpb@gmail.com>
2024-08-21 13:02:40 +0300

    Refactor:refactoring main.go

--- main.go
+++ main.go
@@ -9,11 +9,12 @@ import (
 
 func main() {
 	http.HandleFunc("/", handlers.HomePageHandler)
+	fmt.Println("Starting server on http://localhost:5000")
 	http.HandleFunc("/ascii-art", handlers.GenerateArt)
-	fmt.Println("Starting server on http://localhost:8000")
-	err := http.ListenAndServe(":8000", nil)
+	port := ":5000"
+	err := http.ListenAndServe(port, nil)
 	if err != nil {
-		fmt.Println("Server failed to start")
+		fmt.Print("Server failed to start succesfully")
 		return
 	}
 }
