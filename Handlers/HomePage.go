606bb7e702843e55740149fc8ebf9ad50ee54148
fnamayi <franklynenm@gmail.com>
2024-08-22 11:58:20 +0300

    fix:(erro handling) handled errors without directing to another page

--- Handlers/HomePage.go
+++ Handlers/HomePage.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 
+// HomePageHandler serves the homepage and handles errors for incorrect methods or paths.
 func HomePageHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("templates/index.html")
 	if err != nil {
@@ -12,13 +13,11 @@ func HomePageHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	if r.URL.Path != "/" {
-		w.WriteHeader(404)
-		http.ServeFile(w, r, "templates/404.html")
+		http.Error(w, "Bad Request", http.StatusBadRequest)
 		return
 	}
 	if r.Method != http.MethodGet {
-		w.WriteHeader(405)
-		http.ServeFile(w, r, "templates/405.html")
+		http.Error(w," ",http.StatusMethodNotAllowed)
 		return
 	}
 	if err := tmpl.Execute(w, nil); err != nil {
